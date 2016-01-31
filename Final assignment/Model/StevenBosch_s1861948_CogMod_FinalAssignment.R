6bbb7312ddb3bfb4043b99348f494111ef2cf8d0
StevenBosch <stevenbosch@live.nl>
2016-01-31 14:42:43 +0100

    bal

--- Final assignment/Model/StevenBosch_s1861948_CogMod_FinalAssignment.R
+++ Final assignment/Model/StevenBosch_s1861948_CogMod_FinalAssignment.R
@@ -157,4 +157,15 @@ for(block in 1:nrBlocks){
   #with(plotDatTemp[plotDatTemp$group==3,],lines(forePeriod,reactionTime,type="b",col=brown,lwd=2,ylim=yrange,xlim=xrange))
 }
 
-       
+# Perfrom statistical tests
+summary(aov(reactionTime[block==1] ~ forePeriod[block==1], data=data))
+summary(aov(reactionTime[block==2] ~ forePeriod[block==2], data=data))
+summary(aov(reactionTime[block==3] ~ forePeriod[block==3], data=data))
+summary(aov(reactionTime[block==4] ~ forePeriod[block==4], data=data))
+summary(aov(reactionTime[block==5] ~ forePeriod[block==5], data=data))
+
+summary(aov(reactionTime[block==1] ~ group[block==1], data=data))
+summary(aov(reactionTime[block==2] ~ group[block==2], data=data))
+summary(aov(reactionTime[block==3] ~ group[block==3], data=data))
+summary(aov(reactionTime[block==4] ~ group[block==4], data=data))
+summary(aov(reactionTime[block==5] ~ group[block==5], data=data))
